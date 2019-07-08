8d736fef60a9f2ed513c84e8565b2d0dbc28f604
Filip Zyskowski <filipzyskowski@gmail.com>
2019-07-08 13:26:13 +0200

    02Palindrome

---
+++ NET/Solutions/02Palindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    public static class _02Palindrome
+    {
+        public static void Do()
+        {
+            string input = Console.ReadLine();
+            int n = input.Length;
+
+            for (int i = 0, j = n - 1; i < n / 2; i++, j--)
+            {
+                if (input[i] != input[j])
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
