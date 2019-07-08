e45d70aaae6b074fd3ffcd42c4355f67517de55c
Filip Zyskowski <filipzyskowski@gmail.com>
2019-07-08 14:50:30 +0200

    02FixAge

--- JS/solutions.js
+++ JS/solutions.js
@@ -1,7 +1,20 @@
-// Optional Power
+// 01 Optional Power
 function optionalPow(a, b) {
 	var result = confirm("Do you agree with this question?");
 	
 	if (result) return Math.pow(a, b);
 	else return Math.pow(b, a);
+}
+
+// 02 Fix Age
+function fixage(arr) {
+	var result = '';
+	
+	arr.forEach(function(a) {
+		if (a >= 18 && a <= 60) {
+			result += a + ", ";
+		}
+	});
+
+	result.substr(0, result.length - 2);
 }
