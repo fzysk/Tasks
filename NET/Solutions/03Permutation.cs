387dd3958398ced6f13e0589427af833b58d84e0
Filip Zyskowski <filipzyskowski@gmail.com>
2019-07-08 13:35:51 +0200

    03Permutation

---
+++ NET/Solutions/03Permutation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions
+{
+    public static class _03Permutation
+    {
+        public static void Do()
+        {
+            int n = 11; // from task
+
+            string firstArr = Console.ReadLine();
+            string secondArr = Console.ReadLine();
+
+            var firstSorted = firstArr.Split(' ').Select(s => int.Parse(s)).OrderBy(i => i).ToArray();
+            var secondSorted = secondArr.Split(' ').Select(s => int.Parse(s)).OrderBy(i => i).ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (firstSorted[i] != secondSorted[i])
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+    }
+}
